af65103543853f0f7ba66569f16e5fce9bee96cd
Arthurgt <arturbabkiewicz@gmail.com>
2023-10-09 22:36:05 +0200

    Commit_main - Project initialization

---
+++ src/test/java/arthurgt/com/github/githubuserapi/GithubUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package arthurgt.com.github.githubuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
